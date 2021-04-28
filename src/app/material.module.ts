728b1af6afcf75720652ef09011f774feba746cd
Carlos Lazarin <carlos.lazarin@gmail.com>
2021-04-28 20:32:41 -0300

    Adding header into application

---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
